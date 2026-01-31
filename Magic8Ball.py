b15384ac6a71d351d63728cec9c7b0e81247b49e
rinku89mahato-tech <rinku89mahato@gmail.com>
2026-01-31 22:08:21 +0000

    Completed Magic8Ball Lab2

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -9,9 +9,17 @@ import random
 def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
+  response = ["Yes", "No.", "I don't know", "May be", "Very doubtful"]
   #Prompt the user for their question.
-
+  question = input("Do you know AI? ")
   #Answer question randomly with one of the options from your earlier list.
+  length = len(response)
+  r = random.random()* length
+  r = int(r)
+  print(r) # int value
+
+  response_1 = response[r]
+  print(response_1)
 
 
 if __name__ == '__main__':

7beaf1e1b3132df7cfae4d1174c7861b219543d9
rinku89mahato-tech <rinku89mahato@gmail.com>
2026-02-01 21:13:32 +0000

    Lab 2-futuretimepy -completed

--- FutureTime.py
+++ FutureTime.py
@@ -1,7 +1,8 @@
 #FutureTime.py
-#Name:
-#Date:
-#Assignment:
+#Name: Rinku Mahato
+#Date: 01/31/2026
+#Assignment: Lab 2
+# Calcaute future - hours , minute and time by adding moreminutes to user inputs to hours and minuts,
 
 # datetime will allow us to access the system date and time.
 import datetime
@@ -9,20 +10,48 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  #print(now)
+  currentHour = (now.hour - 6) % 24
   currentMinute = now.minute
 
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
+  #print (f"currentHour: {currentHour} , currentMinute: {currentMinute}") #this is just for checking,
 
-  #TODO:
+  #calculating more minuts and hours from moreminutes
+  moreminute = 20
+
+  futureminute = (currentMinute + moreminute) % 60
+  extraHour = (currentMinute + moreminute) // 60
+
+  #print(f"futureminute: {futureminute}")
+  #print(f"extraHour: {extraHour}")
+
+  ####TODO:
   #Ask user for hours
+  inputhours = int(input("enter hours:"))
   #Ask user for minutes
+  inputminutes = int(input("enter minutes:"))
 
-  #Calculate the time after the user-supplied time has passed.
+  ####Calculate the time after the user-supplied time has passed.
 
+  new_min = (inputminutes + futureminute) % 60
+  
+  #print(f"new future minute: {new_min}")
+
+  new_extra_hour = (inputminutes + futureminute) // 60
+
+  #print(f"new_extra_hour: {new_extra_hour}")
+
+  new_hour = (currentHour + inputhours + extraHour + new_extra_hour) % 24
+
+  print(f"future hour: {new_hour}")
+  print(f"future minute: {new_min}")
+  
   #Do not use any if statements in calculating the time.
 
-  #Output the future time in standard format "HH:MM"
+  ####Output the future time in standard format "HH:MM"
+
+  future_time = str(new_hour) + ":" + str(new_min)
+  print(f"future time: {future_time}")
 
 
 if __name__ == '__main__':
